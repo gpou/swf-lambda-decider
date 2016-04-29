b8a9d916503535035c0c0bf83f1fe37157b64891
Gemma Pou <gemma@vreasy.com>
2016-04-29 09:43:40 +0200

    Do not fail workflow execution if there are no decisions

--- index.js
+++ index.js
@@ -40,31 +40,32 @@ module.exports = function (workflowFunction) {
       return;
     }
 
-    if(!d.decisions) {
-      console.log("No decision sent and no decisions scheduled !");
-
-      swf.respondDecisionTaskCompleted({
-        taskToken: event.taskToken,
-        decisions: [{
-           "decisionType": "FailWorkflowExecution",
-           "failWorkflowExecutionDecisionAttributes": {
-              "reason": "No decision sent and no decisions scheduled !",
-              "details": ""
-           }
-        }]
-      }, function (err) {
-         if (err) { console.error(err); return; }
-         console.log("Workflow marked as failed ! (decision task)");
-         context.succeed({err: "Workflow marked as failed ! No decision sent and no decisions scheduled"});
-      });
-
-      return;
-    }
-
     var params = {
       taskToken: event.taskToken,
       decisions: d.decisions
     };
+
+    // When a child workflow is executed, we receive a new decision task after the event
+    // StartChildWorkflowExecutionInitiated is added, which means that the child workflow
+    // was successful to start. But on this situation, we may want to just wait for the
+    // child workflow to finish, so no decisions must be made for now.
+    // It's the decider responsibility to choose when he must fail the workflow, so if no decisions
+    // have been added, just send a DecisionTaskCompleted with no decisions
+    if(!d.decisions) {
+      console.log("No decision sent and no decisions scheduled !");
+      swf.respondDecisionTaskCompleted(params, function(err, data) {
+        if (err) {
+          console.log('Error in respondDecisionTaskCompleted');
+          console.log(err, err.stack); // an error occurred
+          context.fail(err);
+          return;
+        }
+
+        console.log('respondDecisionTaskCompleted results : ', data);
+        context.succeed(data);
+      });
+      return;
+    }
 
     console.log('Workflow function ok, sending '+d.decisions.length+' decisions...');
     console.log(JSON.stringify(params));
